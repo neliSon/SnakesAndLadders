1566e5caef1a52e42da84cd16dd52acea721159a
neliSun <nelson.chow91.nc@gmail.com>
2017-03-15 22:09:21 -0700

    Initial Commit

---
+++ SnakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesAndLadders
+//
+//  Created by Nelson Chow on 2017-03-15.
+//  Copyright © 2017 Nelson Chow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
